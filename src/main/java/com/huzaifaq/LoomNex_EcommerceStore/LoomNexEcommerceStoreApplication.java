2bcabc54c640fc248ad7df145ef5481a7b00fb21
Huzaifa Quadri <huzaifaquadri1853@gmail.com>
2025-08-31 16:06:27 +0530

    Project Setup

---
+++ src/main/java/com/huzaifaq/LoomNex_EcommerceStore/LoomNexEcommerceStoreApplication.java
@@ -0,0 +1,13 @@
+package com.huzaifaq.LoomNex_EcommerceStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoomNexEcommerceStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoomNexEcommerceStoreApplication.class, args);
+	}
+
+}
